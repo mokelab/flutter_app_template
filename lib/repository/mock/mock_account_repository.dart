256a99c4419ac3e1a445dd6fcaea971945f7d3a8
Hirokazu Fukami <fkm@mokelab.com>
2023-03-30 13:37:08 +0900

    Add test for splash screen

--- lib/repository/mock/mock_account_repository.dart
+++ lib/repository/mock/mock_account_repository.dart
@@ -2,9 +2,15 @@ import 'package:template/model/account.dart';
 import 'package:template/repository/account_repository.dart';
 
 class MockAccountRepository implements AccountRepository {
+  Account? loginWithStoredTokenResult;
+  Exception? loginWithStoredTokenError;
+
   @override
   Future<Account?> loginWithStoredToken() async {
-    return null;
+    if (loginWithStoredTokenError != null) {
+      throw loginWithStoredTokenError!;
+    }
+    return loginWithStoredTokenResult;
   }
 
   @override
---
+++ test/screen/splash/splash_viewmodel_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:template/model/account.dart';
+import 'package:template/repository/mock/mock_account_repository.dart';
+import 'package:template/screen/splash/splash_viewmodel.dart';
+
+void main() {
+  test("setup_first_launch", () async {
+    final accountRepository = MockAccountRepository();
+    accountRepository.loginWithStoredTokenResult = null;
+
+    final viewModel = SplashViewModel(accountRepository);
+    await viewModel.setup();
+    expect(viewModel.uiState, UiState.showLogin);
+  });
+
+  test("setup_already_login", () async {
+    final accountRepository = MockAccountRepository();
+    accountRepository.loginWithStoredTokenResult =
+        const Account(accountId: "account1");
+
+    final viewModel = SplashViewModel(accountRepository);
+    await viewModel.setup();
+    expect(viewModel.uiState, UiState.showTop);
+  });
+
+  test("setup_error", () async {
+    final accountRepository = MockAccountRepository();
+    accountRepository.loginWithStoredTokenError = Exception("API call error");
+
+    final viewModel = SplashViewModel(accountRepository);
+    await viewModel.setup();
+    expect(viewModel.uiState, UiState.showLogin);
+  });
+}
