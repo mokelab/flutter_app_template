3365cdd8cb25e63ce3b1d3e25c61212110861cf2
Hirokazu Fukami <fkm@mokelab.com>
2023-04-03 14:43:47 +0900

    Add test for ViewModel

--- lib/repository/mock/mock_article_repository.dart
+++ lib/repository/mock/mock_article_repository.dart
@@ -3,9 +3,13 @@ import 'package:template/repository/article_repository.dart';
 
 class MockArticleRepository implements ArticleRepository {
   List<Article>? getListResult;
+  Exception? getListError;
 
   @override
   Future<List<Article>> getList(DateTime start) async {
+    if (getListError != null) {
+      throw getListError!;
+    }
     return getListResult!;
   }
 }
