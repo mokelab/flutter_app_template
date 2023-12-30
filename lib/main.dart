7212003813a0913c169fa39b44cd700b2a41514a
Hirokazu Fukami <fkm@mokelab.com>
2023-12-31 08:16:08 +0900

    Remove provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:provider/provider.dart' as p;
-import 'app_module.dart';
 
 import 'mock_module.dart';
 import 'routes.dart';
